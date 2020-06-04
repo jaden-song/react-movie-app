5fd5b99bee407f52188b7158acc4f3cb2c4e5c25
jaden-song <seoA2016##>
2020-06-05 00:18:57 +0900

    Section 2 End

--- README.md
+++ README.md
@@ -2,3 +2,19 @@
 
 React JS Fundamental Course by nomadcoders
 
+# 설치 프로그램
+* node
+* npm
+* npx
+* git
+* visual studio code
+
+# 프로젝트 시작
+* npx create-react-app <project-name>
+
+# 알게 된 것
+* array.map 을 이용하여 component 호출
+* jsx = html + javascript
+* component 펑션 인자 검증
+    * npm i prop-types
+    * 펑션.propTypes{}; 로 각 인자값 검증 처리
--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from 'prop-types';
+
+const iuArr = [
+  {
+    id: 1,
+    num: 20,
+    name: "iu1",
+    image: "http://res.heraldm.com/phpwas/restmb_jhidxmake.php?idx=5&simg=201809121609186592573_20180912161043_01.jpg"
+  },
+  {
+    id: 2,
+    num: 30,
+    name: "iu2",
+    image: "https://i.pinimg.com/474x/b2/b7/2c/b2b72cfb5445a43207c648bb443ee605--modern-times-kpop-girls.jpg"
+  },
+  {
+    id: 3,
+    num: 40,
+    name: "iu3",
+    image: "https://pm1.narvii.com/7503/07a6aac3ca9ac474c224728cc20f185dcd8c5886r1-1080-1350v2_uhq.jpg"
+  },
+];
+
+function IU({ name, img, num }) {
+  return (
+    <div>
+      <h2>I like {name} : {num}</h2>
+      <img src={img} alt={name} />
+    </div>
+  );
+}
+
+IU.propTypes = {
+  name: PropTypes.string.isRequired,
+  img: PropTypes.string.isRequired,
+  num: PropTypes.number.isRequired
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {iuArr.map(iu => (
+        <IU 
+          key={iu.id} 
+          num={iu.num} 
+          name={iu.name} 
+          img={iu.image} 
+        />
+      ))}
     </div>
   );
 }
