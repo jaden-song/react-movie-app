f3d8548193990625589d1c8bf7112c64d5db9a85
jaden-song <seoA2016##>
2020-06-05 10:47:36 +0900

    Section 4 End

--- README.md
+++ README.md
@@ -8,13 +8,26 @@ React JS Fundamental Course by nomadcoders
 * npx
 * git
 * visual studio code
-
+* 영화 리스트 사이트 : https://yts-proxy.now.sh/list_movies.json
 # 프로젝트 시작
 * npx create-react-app <project-name>
 
 # 알게 된 것
 * array.map 을 이용하여 component 호출
 * jsx = html + javascript
-* component 펑션 인자 검증
+* function component 인자 검증
     * npm i prop-types
     * 펑션.propTypes{}; 로 각 인자값 검증 처리
+* Lifecycle of class Component (extends React.Component)
+    * Mounting
+        * constructor()
+        * render()
+        * componentDidMount() - 최초에 한번
+    * Updating
+        * render()
+        * componentDidUpdate()
+    * Unmounting
+        * componentWillUnmount()
+* axios
+    * npm i axios
+* state 가 필요하다면 class 아니면 function 으로 처리
--- src/App.js
+++ src/App.js
@@ -1,55 +1,61 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import axios from 'axios';
+import Movie from './Movie';
+import "./App.css";
 
-const iuArr = [
-  {
-    id: 1,
-    num: 20,
-    name: "iu1",
-    image: "http://res.heraldm.com/phpwas/restmb_jhidxmake.php?idx=5&simg=201809121609186592573_20180912161043_01.jpg"
-  },
-  {
-    id: 2,
-    num: 30,
-    name: "iu2",
-    image: "https://i.pinimg.com/474x/b2/b7/2c/b2b72cfb5445a43207c648bb443ee605--modern-times-kpop-girls.jpg"
-  },
-  {
-    id: 3,
-    num: 40,
-    name: "iu3",
-    image: "https://pm1.narvii.com/7503/07a6aac3ca9ac474c224728cc20f185dcd8c5886r1-1080-1350v2_uhq.jpg"
-  },
-];
+class App extends React.Component {
+    state = {
+        isLoading: true,
+        movies: []
+    };
 
-function IU({ name, img, num }) {
-  return (
-    <div>
-      <h2>I like {name} : {num}</h2>
-      <img src={img} alt={name} />
-    </div>
-  );
-}
+    getMovies = async () => {
+        const {
+            data: {
+                data: {
+                    movies
+                }
+            }
+        } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+        this.setState({movies, isLoading: false});
+    }
 
-IU.propTypes = {
-  name: PropTypes.string.isRequired,
-  img: PropTypes.string.isRequired,
-  num: PropTypes.number.isRequired
-};
+    componentDidMount() {
+        this.getMovies();
+    };
 
-function App() {
-  return (
-    <div>
-      {iuArr.map(iu => (
-        <IU 
-          key={iu.id} 
-          num={iu.num} 
-          name={iu.name} 
-          img={iu.image} 
-        />
-      ))}
-    </div>
-  );
+    render() {
+        const {isLoading, movies} = this.state;
+        return (
+            <section className="container">
+                {
+                    isLoading
+                        ? (
+                            <div className="loader">
+                                <span className="loader__text">"Loading..."</span>
+                            </div>
+                        )
+                        : (
+                            <div className="movies">
+                                {
+                                    movies.map(movie => (
+                                        <Movie
+                                            key={movie.id}
+                                            id={movie.id}
+                                            year={movie.year}
+                                            title={movie.title}
+                                            summary={movie.summary}
+                                            poster={movie.medium_cover_image}
+                                            genres={movie.genres}
+                                          />
+                                    ))
+                                }
+                            </div>
+                        )
+                }
+            </section>
+        );
+    }
 }
 
 export default App;
